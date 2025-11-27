496c0ad90aae3d51a331831ee0d573b0a386ae3b
Padhiver <padhiver@vivaldi.net>
2025-11-27 12:08:00 +0100

    Update action imbriquée
    Update action imbriquée

--- babele-register.js
+++ babele-register.js
@@ -1,9 +1,29 @@
 Hooks.once('init', async function() {
   if (typeof Babele !== 'undefined') {
+    
+    // 1. Enregistrement du module et du dossier de traduction
     game.babele.register({
       module: 'crucible-fr',
       lang: 'fr',
       dir: 'compendium/fr'
+    });
+
+    // 2. Converters
+    Babele.get().registerConverters({
+        "actions_converter": (actions, translations) => {
+            if (!actions || !translations) return actions;
+            
+            return actions.map(item => {
+                const translation = translations[item.id];
+                
+                if (translation) {
+                    if (translation.name) item.name = translation.name;
+                    if (translation.description) item.description = translation.description;
+                    if (translation.condition) item.condition = translation.condition;
+                }
+                return item;
+            });
+        }
     });
 
     console.log('Crucible FR | Module de traduction chargé');
